7112a9e03f43a419754ea39d34bb23be288c6fdc
Muhammed Swalih M <zzray485@gmail.com>
2025-02-15 20:45:31 +0530

    215. Kth Largest Element in an Array added

---
+++ Heap/215.py
@@ -0,0 +1,14 @@
+#215. Kth Largest Element in an Array
+import heapq
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
+f = Solution()
+a = [3,2,1,5,6,4]
+k =2
+print(f.findKthLargest(a, k))
